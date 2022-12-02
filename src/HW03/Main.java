45141c0750694005f5b14aae2acb5c4a4cd8963f
Vlad <vladot@ya.ru>
2022-12-02 13:30:54 +0600

    Task 1

--- src/HW03/Main.java
+++ src/HW03/Main.java
@@ -3,5 +3,30 @@ package HW03;
 public class Main {
     public static void main(String[] args) {
 
+        task1();
+        
     }
+    public static void task1(){
+
+        System.out.println("Задача 1");
+        System.out.println();
+
+        byte one = 8;
+        int two = 16;
+        short three = 32;
+        long four = 64L;
+        float five = 128F;
+        double six = 512D;
+
+        System.out.println("Значение переменной one с типом byte равно " + one);
+        System.out.println("Значение переменной two с типом int равно " + two);
+        System.out.println("Значение переменной three с типом short равно " + three);
+        System.out.println("Значение переменной four с типом long равно " + four);
+        System.out.println("Значение переменной five с типом float равно " + five);
+        System.out.println("Значение переменной six с типом double равно " + six);
+
+        System.out.println();
+        System.out.println();
+    }
+
 }
